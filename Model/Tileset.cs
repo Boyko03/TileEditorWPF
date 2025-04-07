60fe13f2017af60499c40981fcd8f91f33b0ab34
Boyko03 <221426@buas.nl>
2025-04-07 12:48:34 +0200

    Set JsonProperty names

--- Model/Tileset.cs
+++ Model/Tileset.cs
@@ -2,28 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TileEditorWPF.Model
 {
     public class Tileset
     {
+        [JsonPropertyName("name")]
         public required string Name { get; set; }
+        [JsonPropertyName("tilewidth")]
         public int TileWidth { get; set; }
+        [JsonPropertyName("tileheight")]
         public int TileHeight { get; set; }
+        [JsonPropertyName("tilecound")]
         public int TileCount { get; set; }
+        [JsonPropertyName("columns")]
         public int Columns { get; set; }
+        [JsonPropertyName("imagewidth")]
         public int ImageWidth { get; set; }
+        [JsonPropertyName("imageheight")]
         public int ImageHeight { get; set; }
+        [JsonPropertyName("margin")]
         public int Margin { get; set; }
+        [JsonPropertyName("spacing")]
         public int Spacing { get; set; }
+        [JsonPropertyName("image")]
         public required string Image { get; set; }
+        [JsonPropertyName("tiles")]
         public List<Tile>? Tiles { get; set; }
     }
 
     public class Tile
     {
+        [JsonPropertyName("name")]
         public int Id { get; set; }
+        [JsonPropertyName("properties")]
         public Dictionary<string, string>? Properties { get; set; }
     }
 }
